3d4b5fcb08aa3dfa9af43d3555f5a4e3eebc8621
Martin Strobel <marstr@microsoft.com>
2017-12-19 13:29:26 -0800

    Adding a first whack...
    but there is some non-determinism that causes occasional failure.

---
+++ merge_finder.go
@@ -0,0 +1,35 @@
+package mirrorcat
+
+import "context"
+
+// MergeFinder allows a mechanism to find mirror mappings from multiple underlying MirrorFinders.
+type MergeFinder []MirrorFinder
+
+// FindMirrors enumerates each MirrorFinder, and
+func (haystack MergeFinder) FindMirrors(ctx context.Context, needle RemoteRef, results chan<- RemoteRef) (err error) {
+	defer close(results)
+
+	for _, finder := range haystack {
+		// Due to the fact that all FindMirrors implementations must close the results channel to communicate
+		// that no more matches have been found, we must create a layer of separation between the merged results
+		// and the results from each child MirrorFinder.
+		intermediate := make(chan RemoteRef)
+
+		errs := make(chan error, 1)
+
+		// Kick-off a goroutine to fetch the child's matching mirrors.
+		go func() {
+			errs <- finder.FindMirrors(ctx, needle, intermediate)
+		}()
+
+		for mirror := range intermediate {
+			results <- mirror
+		}
+
+		err = <-errs
+		if err != nil {
+			return
+		}
+	}
+	return
+}
---
+++ merge_finder_test.go
@@ -0,0 +1,82 @@
+package mirrorcat_test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/Azure/mirrorcat"
+)
+
+func ExampleMergeFinder_FindMirrors() {
+	const mainRepo = "github.com/Azure/mirrorcat"
+	const secondaryRepo = "github.com/marstr/mirrorcat"
+
+	child1, child2 := mirrorcat.NewDefaultMirrorFinder(), mirrorcat.NewDefaultMirrorFinder()
+
+	orig := mirrorcat.RemoteRef{Repository: mainRepo, Ref: "master"}
+
+	child1.AddMirrors(orig, mirrorcat.RemoteRef{Repository: secondaryRepo, Ref: "master"})
+	child2.AddMirrors(orig, mirrorcat.RemoteRef{Repository: mainRepo, Ref: "dev"})
+
+	subject := mirrorcat.MergeFinder([]mirrorcat.MirrorFinder{child1, child2})
+
+	results := make(chan mirrorcat.RemoteRef)
+
+	var err error
+	go func() {
+		err = subject.FindMirrors(context.Background(), orig, results)
+	}()
+
+	for result := range results {
+		fmt.Printf("%s:%s\n", result.Repository, result.Ref)
+	}
+	fmt.Println(err)
+
+	// Output:
+	// github.com/marstr/mirrorcat:master
+	// github.com/Azure/mirrorcat:dev
+	// <nil>
+}
+
+func TestMergeFinder_RespectsCancel(t *testing.T) {
+	const mainRepo = "github.com/Azure/mirrorcat"
+	const secondaryRepo = "github.com/marstr/mirrorcat"
+
+	child1, child2 := mirrorcat.NewDefaultMirrorFinder(), mirrorcat.NewDefaultMirrorFinder()
+
+	orig := mirrorcat.RemoteRef{Repository: mainRepo, Ref: "master"}
+
+	child1.AddMirrors(orig, mirrorcat.RemoteRef{Repository: secondaryRepo, Ref: "master"})
+	child2.AddMirrors(orig, mirrorcat.RemoteRef{Repository: mainRepo, Ref: "dev"})
+
+	subject := mirrorcat.MergeFinder([]mirrorcat.MirrorFinder{child1, child2})
+
+	outside, cancelOutside := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancelOutside()
+
+	inside, cancelInside := context.WithCancel(outside)
+
+	results := make(chan mirrorcat.RemoteRef)
+
+	errs := make(chan error, 1)
+
+	go func() {
+		errs <- subject.FindMirrors(inside, orig, results)
+	}()
+
+	cancelInside()
+
+	select {
+	case err := <-errs:
+		t.Log("Error received: ", err)
+		if err == nil || !strings.Contains(err.Error(), "cancel") {
+			t.Logf("expected error to be a cancellation message")
+			t.Fail()
+		}
+	case <-outside.Done():
+		t.Errorf("timed out")
+	}
+}
