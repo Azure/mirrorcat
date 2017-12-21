b1911aed3993b1cf599b6140be1bbe2037fde2a9
Martin Strobel <marstr@microsoft.com>
2017-12-21 10:40:41 -0800

    Adding test for RedisFinder_FindMirrors

--- redis_finder_test.go
+++ redis_finder_test.go
@@ -1,10 +1,14 @@
 package mirrorcat_test
 
 import (
+	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/Azure/mirrorcat"
+	"github.com/go-redis/redis"
+	"github.com/spf13/viper"
 )
 
 func ExampleRedisRemoteRef_String() {
@@ -84,5 +88,90 @@ func TestParseRedisRemoteRef_Invalid(t *testing.T) {
 				t.Fail()
 			}
 		})
+	}
+}
+
+func TestRedisFinder_FindMirrors(t *testing.T) {
+	viper.BindEnv("redis-connection", "MIRRORCAT_REDIS_CONNECTION")
+	viper.SetDefault("redis-connection", "redis://localhost:6379")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	t.Log("using redis connection:", viper.GetString("redis-connection"))
+	connectionOptions, err := redis.ParseURL(viper.GetString("redis-connection"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const testKey = "master:testRepo"
+	expected := map[mirrorcat.RemoteRef]struct{}{
+		mirrorcat.RemoteRef{Repository: "testRepo", Ref: "dev"}:  struct{}{},
+		mirrorcat.RemoteRef{Repository: "otherRepo", Ref: "dev"}: struct{}{},
+	}
+
+	expectedArr := make([]interface{}, 0, len(expected))
+	for item := range expected {
+		expectedArr = append(expectedArr, mirrorcat.RedisRemoteRef(item).String())
+	}
+
+	client := redis.NewClient(connectionOptions)
+	_, err = client.SAdd(testKey, expectedArr...).Result()
+	defer func() {
+		_, err = client.Del(testKey).Result()
+		if err != nil {
+			t.Log("Unable to cleanup Redis Instance: ", err)
+		} else {
+			t.Log("Redis Instance cleaned up.")
+		}
+	}()
+
+	if err != nil {
+		t.Log("Unable to connect to Redis instance: ", err)
+		t.SkipNow()
+	}
+
+	subject := mirrorcat.RedisFinder(*client)
+
+	testRepo, err := mirrorcat.ParseRedisRemoteRef(testKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	results, errs := make(chan mirrorcat.RemoteRef), make(chan error, 1)
+
+	go func() {
+		select {
+		case errs <- subject.FindMirrors(ctx, mirrorcat.RemoteRef(testRepo), results):
+		case <-ctx.Done():
+			errs <- ctx.Err()
+		}
+	}()
+
+loop:
+	for {
+		select {
+		case err = <-errs:
+			if err != nil {
+				t.Fatal(err)
+			}
+		case seen, ok := <-results:
+			if !ok {
+				break loop
+			}
+
+			_, ok = expected[seen]
+			if ok {
+				delete(expected, seen)
+			} else {
+				t.Log("unexpected result: ", seen)
+				t.Fail()
+			}
+		}
+	}
+
+	for unseen := range expected {
+		t.Log("didn't see expected value: ", unseen)
+		t.Fail()
 	}
 }
